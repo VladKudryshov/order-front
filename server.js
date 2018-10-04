59b9c5610282eeca1ec97cca0c2d12aff56d903f
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2018-10-04 14:55:30 +0300

    update server

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
