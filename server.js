cc0da583175994c299fade215bb64f975365149c
kudr9tov <uladzislau.kudrashou@instinctools.ru>
2018-10-04 13:43:10 +0300

    update start

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-const app = express();
+var CONTACTS_COLLECTION = "contacts";
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+var app = express();
+app.use(bodyParser.json());
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
